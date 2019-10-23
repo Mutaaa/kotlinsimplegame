6fa57d661c890321ce3f367f1069a034d788ce4c
Kornkanok S <k.sangwichien@gmail.com>
2019-10-23 18:02:35 +0300

    full screen, no title

--- 1verProject/app/src/main/java/com/example/androidgame/MainActivity.kt
+++ 1verProject/app/src/main/java/com/example/androidgame/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.androidgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
+
+        if (supportActionBar != null)
+            supportActionBar?.hide()
+
     }
 }
